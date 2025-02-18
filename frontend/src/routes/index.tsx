28a2c569be55fc81ae39cb672e1dd717bce90dc0
breachr <brianhoang873@gmail.com>
2025-02-17 21:02:30 -0800

    Shadcn and tailwindcss setup

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Alert, AlertTitle, AlertDescription } from "../components/ui/alert";
+import { Terminal } from "lucide-react";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div>
+      <Alert >
+        <Terminal className="h-6 w-6" />
+        <AlertTitle className=" text-3xl">Heads up!</AlertTitle>
+        <AlertDescription className=" text-3xl">
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
-  )
+  );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,21 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import viteReact from "@vitejs/plugin-react-swc"
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
+    tailwindcss(),
     // ...,
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});

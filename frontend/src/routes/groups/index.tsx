fdf0455523be73eeb6d3a05e704b2c731f5aa5aa
breachr2 <brianhoang873@gmail.com>
2025-02-25 17:25:38 -0800

    Add link to groups page

--- frontend/src/routes/groups/index.tsx
+++ frontend/src/routes/groups/index.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { Terminal } from "lucide-react";
 
-export const Route = createFileRoute('/groups/')({
+export const Route = createFileRoute("/groups/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/groups/"!</div>
+  return (
+    <div>
+      <Link to="/" className="underline text-blue-500 text-lg">Home</Link>
+      <Alert variant={"destructive"}>
+        <Terminal />
+        <AlertTitle className="text-xl">Group Page</AlertTitle>
+        <AlertDescription className="text-xl">Groups Go Here</AlertDescription>
+      </Alert>
+    </div>
+  );
 }
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -10,15 +10,24 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <div>
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-      <Link to="/groups/$groupId/messages" params={{groupId : "1"}} className="[&.active]:font-bold">
-        Messages
-      </Link>
+      <div className="flex gap-4 text-blue-600 underline">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+        <Link to="/groups" className="[&.active]:font-bold">
+          Groups
+        </Link>
+        <Link
+          to="/groups/$groupId/messages"
+          params={{ groupId: "1" }}
+          className="[&.active]:font-bold"
+        >
+          Messages
+        </Link>
+      </div>
       <Alert>
         <Terminal className="h-6 w-6" />
         <AlertTitle className=" text-3xl">Heads up!</AlertTitle>

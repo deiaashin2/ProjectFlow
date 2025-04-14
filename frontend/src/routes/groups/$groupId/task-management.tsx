7180d801bf79d54ee75cae3264e4641b568109f4
Andreia S <deiaashin@gmail.com>
2025-04-14 14:02:21 -0700

    Task management done

--- frontend/src/routes/groups/$groupId/task-management.tsx
+++ frontend/src/routes/groups/$groupId/task-management.tsx
@@ -65,6 +65,17 @@ import {
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import AppSidebar from "@/components/app-sidebar";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { useState } from 'react';
 
 export const Route = createFileRoute("/groups/$groupId/task-management")({
   component: MessagePage,
@@ -295,7 +306,7 @@ export const columns: ColumnDef<Task>[] = [
                 Edit Task
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem>View Task Details</DropdownMenuItem>
+              <SheetDemo/>
             <DropdownMenuItem>Delete</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
@@ -509,4 +520,42 @@ export function CardWithForm() {
       </CardFooter>
     </Card>
   );
+}
+
+export function SheetDemo() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+      <DropdownMenuItem onClick={() => setIsOpen(true)}>Task details</DropdownMenuItem>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit" onClick={() => setIsOpen(false)}>Save changes</Button> 
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
 }

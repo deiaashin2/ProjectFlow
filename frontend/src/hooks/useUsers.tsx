e8616f49524b4a229ed99164a10f68f95c225ee3
breachr2 <brianhoang873@gmail.com>
2025-05-05 16:30:00 -0700

    Fixed messages page to use mock api endpoint for users

--- frontend/src/hooks/useUsers.tsx
+++ frontend/src/hooks/useUsers.tsx
@@ -1,5 +1,4 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { API_BASE_URL } from "@/lib/constants";
 
 export type User = {
   username: string;
@@ -14,7 +13,7 @@ export type User = {
 };
 
 async function getUsers(): Promise<User[]> {
-  const response = await fetch(`${API_BASE_URL}/users`);
+  const response = await fetch("https://67c79ebcc19eb8753e7a2f85.mockapi.io/api/v1/users");
 
   if (!response.ok) {
     throw new Error("Error fetching users");
